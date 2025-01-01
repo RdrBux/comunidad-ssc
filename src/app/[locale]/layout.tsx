9fa6291098b6b78292dffdd1d626ef182f1aa4a8
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2025-01-01 13:18:13 -0300

    Remove course sticky banner

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,7 +5,7 @@ import { sourceSerif } from "@/utils/fonts";
 import { Analytics } from '@vercel/analytics/react';
 import { getMessages, getTranslations } from "next-intl/server";
 import { NextIntlClientProvider } from 'next-intl';
-import StickyBanner from "@/components/sticky-banner";
+/* import StickyBanner from "@/components/sticky-banner"; */
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -45,7 +45,7 @@ export default async function RootLayout({
     <html lang={locale}>
       <body className={`${inter.className} ${sourceSerif.variable} tracking-tight antialiased`}>
         <NextIntlClientProvider messages={messages}>
-          <StickyBanner />
+          {/* <StickyBanner /> */}
           <div className="relative">{children}</div>
         </NextIntlClientProvider>
         <Analytics />
